142067fbadcf2e88d2c78d8b43aeb9c1bbabb3de
Will <wmccrory@gmail.com>
2018-02-03 13:25:28 -0600

    Finished layout of page. Setting up javascript and firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAlsEFXDl4CfJaZbKG3pj_eeTQ_PB8ygAs",
+    authDomain: "traintime-5749c.firebaseapp.com",
+    databaseURL: "https://traintime-5749c.firebaseio.com",
+    projectId: "traintime-5749c",
+    storageBucket: "",
+    messagingSenderId: "1054949144448"
+  };
+  firebase.initializeApp(config);
